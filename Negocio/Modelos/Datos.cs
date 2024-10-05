4aabc063b922a38e13e5696391cba083726756a8
CamilaVega <caamilavega@gmail.com>
2024-10-05 19:51:12 -0300

    Comienzo del proyecto agregando la clase Datos y el controlador API, donde se les da funcionalidad al método Get para traer todos los productos y el GET(id) para elegir el producto filtrando por id

---
+++ Negocio/Modelos/Datos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Modelos
+{
+      public class Datos
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public int price { get; set; }
+
+        public static List<Datos> datosList = new List<Datos>
+        {
+            new Datos {Id=1,title="Mesa",price=10000},
+            new Datos {Id=2,title="Silla",price=6500}
+        };
+    }
+}
---
+++ WebAPI/Controllers/API.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Negocio.Modelos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API : ControllerBase
+    {
+        
+        [HttpGet]
+        public List<Datos> Get()
+        {
+            return Datos.datosList;
+        }
+        
+        [HttpGet("{id}")]
+        public Datos GetDatos(int id)
+        {
+
+            return Datos.datosList.FirstOrDefault(d=>d.Id==id);
+        }
+
+        // POST api/<API>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<API>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<API>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
